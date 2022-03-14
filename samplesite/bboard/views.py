b4c43ff2c9b7bfa1b1b05d58f2934a49cb354b38
Ihar Shauchuk <shauchuk.ihar@gmail.com>
2022-03-14 15:31:22 +0300

    feat: added links between models, string representation of the model

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from .models import Bb
+from .models import Bb, Rubric
 
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    content = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', content)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    content = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', content)
+
